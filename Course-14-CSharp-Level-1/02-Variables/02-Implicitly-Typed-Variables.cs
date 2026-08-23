e8756290d63ee61d76d7fa74f520fc83d34c554c
Manar Alwahedi <alwmanar@gmail.com>
2026-08-23 20:59:55 +0300

    Add implicitly typed variables example

---
+++ Course-14-CSharp-Level-1/02-Variables/02-Implicitly-Typed-Variables.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var x = 10;
+            var y = 89.2;
+            var z = "Manar";
+            Console.WriteLine("x={0}, y={1}, z={2}", x, y, z);
+        }
+    }
+}
