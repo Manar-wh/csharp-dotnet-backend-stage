ac4122be02098eab97256ac2faaa830d17d1b34c
Manar Alwahedi <alwmanar@gmail.com>
2026-08-23 19:04:25 +0300

    Add variables example

---
+++ Course-14-CSharp-Level-1/02-Variables/01-Variables.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Name = "Manar Alwahedi";
+            Console.WriteLine(Name);
+
+            int x = 10 ;
+            int y = 20 ;
+            Console.WriteLine("x= "+ x);
+            Console.WriteLine("y= "+ y);
+            Console.WriteLine("x+y= " + (x + y));
+
+            //other commondata type
+            double MyDouble = 19.33D;
+            char MyLetter = 'M';
+            bool MyBool = true;
+        }
+    }
+}
