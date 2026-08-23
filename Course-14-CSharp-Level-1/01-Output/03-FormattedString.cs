5504c850983b80fc9697fc6343cee18f414cfc28
Manar Alwahedi <alwmanar@gmail.com>
2026-08-23 17:35:40 +0300

    Add formatted string examples

---
+++ Course-14-CSharp-Level-1/01-Output/03-FormattedString.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to {0}, {1}", "Jeddah", "Saudi Arabia");
+            Console.WriteLine("My Name is {0} {1}", "Manar", "Alwahedi");
+            Console.ReadKey();
+        }
+    }
+}
