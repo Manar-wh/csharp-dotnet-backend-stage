914c000f90da758dba3276beb2a416a94de66266
Manar Alwahedi <alwmanar@gmail.com>
2026-08-23 17:25:06 +0300

    Add Write output example

---
+++ Course-14-CSharp-Level-1/01-Output/02-Write.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+            Console.Write(" My name is Manar Alwahedi");
+            Console.Write(" I'm learning C#");
+            Console.ReadKey();
+        }
+    }
+}
