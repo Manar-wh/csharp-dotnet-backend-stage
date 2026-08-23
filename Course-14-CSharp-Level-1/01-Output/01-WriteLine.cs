91f489b4a111162ce5b1343ae87ef9922963b313
Manar Alwahedi <alwmanar@gmail.com>
2026-08-23 17:05:42 +0300

    Add WriteLine output example

---
+++ Course-14-CSharp-Level-1/01-Output/01-WriteLine.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World :)");
+            Console.WriteLine("My name is Manar Alwahedi");
+            Console.WriteLine("I'm learning C#");
+            Console.WriteLine("The sum of 10 + 20 = " + (10 + 20));
+            Console.ReadKey();
+        }
+    }
+}
