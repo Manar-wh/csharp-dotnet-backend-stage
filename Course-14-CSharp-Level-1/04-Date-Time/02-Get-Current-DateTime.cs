9cc9599369656d0bcf0e5e0082eba17da1467228
Manar Alwahedi <alwmanar@gmail.com>
2026-08-28 01:18:29 +0300

    Add current DateTime example

---
+++ Course-14-CSharp-Level-1/04-Date-Time/02-Get-Current-DateTime.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CSharp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt1 = DateTime.Now;
+            Console.WriteLine(dt1);
+            Console.ReadKey();
+        }
+    }
+}
