02052ddef3c4c4b1b36a441400a0d9e6c77e8b7c
Manar Alwahedi <alwmanar@gmail.com>
2026-08-29 01:43:37 +0300

    Add explicit casting example

---
+++ Course-14-CSharp-Level-1/06-Type-Casting/02-Explicit-Casting.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace CSharp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d1 = 33.92;
+            int Myint = (int)d1;
+
+            Console.WriteLine(d1);
+            Console.WriteLine(Myint);
+
+        }
+    }
+}
+
