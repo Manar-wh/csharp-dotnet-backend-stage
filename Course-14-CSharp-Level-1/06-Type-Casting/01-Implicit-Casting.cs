73e6facdf1d2789cff485ccbf5b142a81db61d49
Manar Alwahedi <alwmanar@gmail.com>
2026-08-29 01:40:05 +0300

    Add implicit casting example

---
+++ Course-14-CSharp-Level-1/06-Type-Casting/01-Implicit-Casting.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CSharp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Myint = 19;
+            double d1 = Myint;
+            Console.WriteLine(d1);
+
+        }
+    }
+}
+
