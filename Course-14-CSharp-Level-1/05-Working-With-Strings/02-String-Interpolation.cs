413ef07f5fc613953a359ce0355efae381753f90
Manar Alwahedi <alwmanar@gmail.com>
2026-08-29 01:26:01 +0300

    Add string interpolation example

---
+++ Course-14-CSharp-Level-1/05-Working-With-Strings/02-String-Interpolation.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace CSharp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = "Manar";
+            string LastName = "Alwahedi";
+            string ID = "20";
+
+            Console.WriteLine($"Mrs: {FirstName} {LastName}, ID: {ID}");
+            Console.ReadKey();
+        }
+    }
+}
+
