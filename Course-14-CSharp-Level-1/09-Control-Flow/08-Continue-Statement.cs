c19614a9dd7762dc839bf1e924eae657319ec801
Manar Alwahedi <alwmanar@gmail.com>
2026-08-30 03:17:09 +0300

    Add continue statement example in for loop

---
+++ Course-14-CSharp-Level-1/09-Control-Flow/08-Continue-Statement.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace CSharp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                if (i == 3)
+                {
+                    continue;
+                }
+                Console.WriteLine($"i = {i}");
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
+
