b441af295dce8a409b4d1d5c670f818eb5eaae65
Manar Alwahedi <alwmanar@gmail.com>
2026-09-02 03:50:29 +0300

    Add basic method creation with parameters example

---
+++ Course-14-CSharp-Level-1/12-Methods/01-Create-And-Call-Method.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp14
+{
+    class Program
+    {
+        static void PrintName(string Name){
+            Console.WriteLine(Name);
+        }
+        static void Main(string[] args)
+        {
+            PrintName("Manar");
+            Console.ReadKey();
+        }
+    }
+}
